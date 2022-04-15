1fb5b691b7d875b1a953c16103a03659a4f64fa9
Łukasz Wartałowicz <wartalowiczl@gmail.com>
2022-04-15 11:33:33 +0200

    Fix accordion

--- src/js/script.js
+++ src/js/script.js
@@ -102,20 +102,20 @@
       menuContainer.appendChild(thisProduct.element);
     }
     initAccordion(){
-      const thisApp = this;
+      const thisProduct = this;
 
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       clickableTrigger.addEventListener('click', function(event){
-        /* prevent default action for event */
+        
         event.preventDefault();
-      /* find active product (product that has active class) */
+    
       const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
+      
       if (activeProduct === thisProduct.element){
         activeProduct.classList.remove('active')
       }
-      /* toggle active class on thisProduct.element */
+      
       thisProduct.element.classList.toggle('active');
     });
   }
